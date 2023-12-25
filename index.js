fcb48503c0c8743b9cbe73f815054d47201fedef
Indrajit Pal <palindrajit10@gmail.com>
2023-12-25 15:52:19 +0530

    updated cors

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const path = require('path');
 const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        credentials: true,
+    },
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
